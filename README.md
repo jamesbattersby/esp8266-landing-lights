ba372d37bb03a55ddefc10754db2b4fbb2a77dd5
James Battersby <44945862+jamesbattersby@users.noreply.github.com>
2018-11-17 17:57:25 +0000

    Include file cull

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 # Landing Lights
 
-This is a simple project that uses an ESP8266, HCSR04 ultrasonic sensor and a strip of WS2811 LEDs to produce a garage parking sensor.  As you
+This is a simple project that uses an ESP8266, HCSR04 ultrasonic sensor and a strip of WS2812B LEDs to produce a garage parking sensor.  As you
 drive towards the garage wall, the LEDs slowly go out and change from green to yellow then red as you get closer.  Eventually, the entire LED
 strip flashes red - indicating that it really is time to stop.
 
--- src/landingLights.cpp
+++ src/landingLights.cpp
@@ -8,38 +8,9 @@
 
 // For FastLed
 #define FASTLED_ALLOW_INTERRUPTS 0
-#include <bitswap.h>
-#include <chipsets.h>
-#include <color.h>
-#include <colorpalettes.h>
-#include <colorutils.h>
-#include <controller.h>
-#include <cpp_compat.h>
-#include <dmx.h>
 #include <FastLED.h>
-#include <fastled_config.h>
-#include <fastled_delay.h>
-#include <fastled_progmem.h>
-#include <fastpin.h>
-#include <fastspi.h>
-#include <fastspi_bitbang.h>
-#include <fastspi_dma.h>
-#include <fastspi_nop.h>
-#include <fastspi_ref.h>
-#include <fastspi_types.h>
-#include <hsv2rgb.h>
-#include <led_sysdefs.h>
-#include <lib8tion.h>
-#include <noise.h>
-#include <pixelset.h>
-#include <pixeltypes.h>
-#include <platforms.h>
-#include <power_mgt.h>
 
 // For OTA
-#include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
 // Config for LEDs
